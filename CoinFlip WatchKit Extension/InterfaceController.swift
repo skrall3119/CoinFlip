da2bd7d31de6db12afb3022e3b210cdf1705539f
skrall3119 <alexjanci@yahoo.com>
2022-02-24 14:49:11 -0600

    Initial Commit

---
+++ CoinFlip WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  CoinFlip WatchKit Extension
+//
+//  Created by Alex Janci on 2/24/22.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
